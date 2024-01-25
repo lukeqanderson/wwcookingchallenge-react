658b34e060e6a4a58e112eaf9927a5d512f69683
Luke Anderson <lukeqanderson@gmail.com>
2024-01-24 17:06:35 -0800

    add authenticated get request test w api gateway

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "wwcookingchallengeAPI": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "wwcookingchallengef6751baf": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "wwcookingchallengeLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/components/AuthenticatedApp/AuthenticatedApp.tsx
+++ src/components/AuthenticatedApp/AuthenticatedApp.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { get } from "aws-amplify/api";
+import { getCurrentUser, fetchAuthSession } from "aws-amplify/auth";
 import "./AuthenticatedApp.css";
 
 const AuthenticatedApp = (props: {
@@ -9,7 +11,35 @@ const AuthenticatedApp = (props: {
 }) => {
   useEffect(() => {
     props.setSelectedNavButton("home", 0);
+    getChallengeData();
   }, [props]);
+
+  async function getChallengeData() {
+    try {
+      let authToken = (await fetchAuthSession()).tokens?.idToken?.toString();
+      let username = (await getCurrentUser()).username?.toString();
+      if (authToken === undefined || username === undefined) throw Error;
+      console.log(authToken);
+      console.log(username);
+      const restOperation = get({
+        apiName: "wwcookingchallengeAPI",
+        path: "/userdata",
+        options: {
+          queryParams: {
+            username: username,
+          },
+          headers: {
+            Authorization: authToken,
+          },
+        },
+      });
+      const response = await restOperation.response;
+      const data = await response.body.json();
+      console.log("GET call succeeded: ", data);
+    } catch (error) {
+      console.log("GET call failed: ", error);
+    }
+  }
 
   const signOut = () => {
     props.setRoute("welcome");
